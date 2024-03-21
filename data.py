e051c5ab4f78da9b4c8f4143e5fca46c2797cc79
Bloomy <okarmamilosz@gmail.com>
2024-03-21 15:52:59 +0100

    Commicik (chyba) skończone :D

---
+++ data.py
@@ -0,0 +1,12 @@
+import requests
+
+parameters = {
+    "amount": 10,
+    "type": "boolean"
+}
+
+question = requests.get(url='https://opentdb.com/api.php', params=parameters)
+question.raise_for_status()
+data = question.json()
+
+question_data = data['results']
